78975d28b23c5a7ff03db6135378a2fc70a16162
Alex Forsythe <alexwforsythe@gmail.com>
2014-11-05 01:12:56 -0600

    added template rendering

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ See more at: https://github.com/zachwill/flask_heroku
 
 import os
 from flask import Flask
+from flask import render_template
 
 
 # Settings
@@ -16,9 +17,11 @@ app = Flask(__name__)
 
 # Routing
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/hello/')
+@app.route('/hello/<name>/')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
