001dfbfe4f7d45dd730134cf0066041f0c41ef0f
Alex Forsythe <alexwforsythe@gmail.com>
2014-11-05 01:10:52 -0600

    added docs and credits

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+
+See more at: https://github.com/zachwill/flask_heroku
+"""
+
 import os
 from flask import Flask
 
+
+# Settings
 app = Flask(__name__)
 
+
+# Routing
 @app.route('/')
 def hello():
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run()
